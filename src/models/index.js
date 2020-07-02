c16ccb3d028f4a5b47299d2fa9b04f80bdc95d90
pabloaaf <palvaf02@estudiantes.unileon.es>
2020-07-02 17:51:40 -0600

    replica set read preference

--- src/models/index.js
+++ src/models/index.js
@@ -4,7 +4,7 @@ const TranscriptModel = require('./transcript.db');
 const UserModel = require('./user.db');
 const VideoModel = require('./video.db');
 
-Mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useFindAndModify: false})
+Mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useFindAndModify: false, replSet: {readPreference: 'ReadPreference.NEAREST'} })
   .then(() => {console.log('[MONGODB]: MongoDB Connected')})
   .catch(error => {console.log('[MONGODB]:', error)});
 
